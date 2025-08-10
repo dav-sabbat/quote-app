4afcd8e155e0c0e07fefa62ea45cc973606a36a5
David <david@macbook-pro-de-david.home>
2025-08-10 16:15:51 +0200

    chore: fresh iOS project

---
+++ QuoteApp/QuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteAppApp.swift
+//  QuoteApp
+//
+//  Created by David on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
